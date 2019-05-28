4717b2f69d49704c0733bdc7c6737f35ee3c748f
silviolimeira <sicemal@gmail.com>
2019-05-28 19:25:13 -0300

    first test - should sum two numbers

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,3 @@
 const routes = require("express").Router();
-const { User } = require("./app/models");
-
-User.create({
-  name: "Silvio",
-  email: "silvio@gmail.com",
-  password_hash: "123456789"
-});
 
 module.exports = routes;

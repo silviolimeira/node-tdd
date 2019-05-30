1782c6c66d848eb06c93b526e4edc5a494d860db
silviolimeira <sicemal@gmail.com>
2019-05-30 20:22:27 -0300

    add generate token

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -13,16 +13,52 @@ describe("Authentication", () => {
     const user = await User.create({
       name: "silvio",
       email: "silvio@gmail.com",
-      password_hash: "123456"
+      password: "123456"
     });
 
     const response = await request(app)
       .post("/sessions")
       .send({
         email: user.email,
-        password: user.password_hash
+        password: "123456" //user.password_hash
       });
 
     expect(response.status).toBe(200);
   });
+
+  it("should autenticate with invalid credentials", async () => {
+    const user = await User.create({
+      name: "silvio",
+      email: "silvio@gmail.com",
+      password: "123456"
+    });
+
+    const response = await request(app)
+      .post("/sessions")
+      .send({
+        email: user.email,
+        password: "12345611" //user.password_hash
+      });
+
+    expect(response.status).toBe(401);
+  });
+
+
+  it('should return jwt toker when authenticated', async () => {
+    const user = await User.create({
+      name: "silvio",
+      email: "silvio@gmail.com",
+      password: "123123"
+    });
+
+    const response = await request(app)
+      .post("/sessions")
+      .send({
+        email: user.email,
+        password: "123123" //user.password_hash
+      });
+
+    expect(response.body).toHaveProperty("token");
+  });
+
 });
--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,6 +1,24 @@
+const { User } = require('../models')
+
 class SessionController {
   async store(req, res) {
-    return res.status(200).send();
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(401).json({ message: 'User not fouond' });
+    }
+
+    if (!(await user.checkPassword(password))) {
+      return res.status(401).json({ message: 'Incorrect password' });
+    }
+
+    //return res.status(200).send();
+    return res.json({ 
+      user,
+      token: user.generateToken() 
+    });
   }
 }
 

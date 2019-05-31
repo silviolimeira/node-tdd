cdd056a7d2c73e7bd4a032566d6aa530e495240f
silviolimeira <sicemal@gmail.com>
2019-05-31 17:51:09 -0300

    should not be able to access private routes with invalid jwt token

--- src/app/middleware/auth.js
+++ src/app/middleware/auth.js
@@ -1,10 +1,26 @@
 const jwt = require('jsonwebtoken');
+const { promisify } = require('util');
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
     const authHeader = req.headers.authorization;
 
     if (!authHeader) {
         return res.status(401).json({ message: 'Token not provided' });
 
     }
+
+    const [, token] = authHeader.split(' ');
+
+    try {
+        const decoded = await promisify(jwt.verify)(token, process.env.APP_SECRET);
+
+        req.userId = decode.id;
+
+        return next()
+    } catch (err) {
+        return res.status(401).json({ message: 'Token invalid' });
+    }
+
+    return next;
+
 }
